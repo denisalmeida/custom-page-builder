cac0b1b43bb576bfa4cb972d9ac8cd287fa02739
denisalmeida <denisalmeida@me.com>
2024-10-23 05:04:16 -0300

    :rocket: launch custom-pagebuilder module

---
+++ registration.php
@@ -0,0 +1,14 @@
+
+<?php
+
+/**
+ * @category  Class
+ * @package   CustomPageBuilder
+ * @version   1.0.0
+ * @copyright Copyright © 2024 Denis Almeida | Ambienz
+ * @author    Denis Almeida (https://www.ambienz.com.br)
+ */
+
+use Magento\Framework\Component\ComponentRegistrar;
+
+ComponentRegistrar::register(ComponentRegistrar::MODULE, 'Ambienz_CustomPageBuilder', __DIR__);
